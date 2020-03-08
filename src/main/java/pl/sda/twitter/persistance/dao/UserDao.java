1136298eae48c3dfa4ed1c4cd9c421ddf3c3cd9c
moira <milena.falka@gmail.com>
2020-03-08 13:05:34 +0100

    UserDao dodanie pobierania uzytkownika bez hql

--- src/main/java/pl/sda/twitter/persistance/dao/UserDao.java
+++ src/main/java/pl/sda/twitter/persistance/dao/UserDao.java
@@ -8,20 +8,35 @@ import pl.sda.twitter.persistance.entities.TbUser;
 
 import javax.persistence.NoResultException;
 import javax.persistence.NonUniqueResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.io.Serializable;
 
 public class UserDao implements Serializable {
     public TbUser getUserByLogin(String login) throws IncorrectLoginOrPasswordException {
+//        try (final Session session = HibernateUtil
+//                .getSESSION_FACTORY()
+//                .openSession()) {
+//            final Query q = session.createQuery("select e from "
+//                    + TbUser.class.getName() + " e where e.login = :username");
+//            q.setParameter("username", login);
+//            session.beginTransaction();
+//            final TbUser singleResult = (TbUser) q.getSingleResult();
+//            return singleResult;
+//        } catch (NoResultException | NonUniqueResultException e){
+//            throw new IncorrectLoginOrPasswordException(e.getMessage());
+//        }
         try (final Session session = HibernateUtil
                 .getSESSION_FACTORY()
                 .openSession()) {
-            final Query q = session.createQuery("select e from "
-                    + TbUser.class.getName() + " e where e.login = :username");
-            q.setParameter("username", login);
-            session.beginTransaction();
-            final TbUser singleResult = (TbUser) q.getSingleResult();
-            return singleResult;
-        } catch (NoResultException | NonUniqueResultException e){
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<TbUser> criteriaQuery = builder.createQuery(TbUser.class);
+            final Root<TbUser> root = criteriaQuery.from(TbUser.class);
+            criteriaQuery.select(root).where(builder.equal(root.get("login"), login));
+            final Query<TbUser> query = session.createQuery(criteriaQuery);
+            return query.getSingleResult();
+        } catch (NoResultException | NonUniqueResultException e) {
             throw new IncorrectLoginOrPasswordException(e.getMessage());
         }
     }
