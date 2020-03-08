3ef56b722328457fd7a32d38736a5818a9145706
moira <milena.falka@gmail.com>
2020-03-08 10:41:56 +0100

    dodanie HubernateUtil

---
+++ src/main/java/pl/sda/twitter/persistance/HibernateUtil.java
@@ -0,0 +1,22 @@
+package pl.sda.twitter.persistance;
+
+import lombok.Getter;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    @Getter
+    private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory(){
+        try{
+            return new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+        } catch(Throwable e){
+            System.err.println("Initial SessionFactory creation failed." + e);
+           throw new ExceptionInInitializerError(e);
+        }
+    }
+
+}
